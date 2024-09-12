b6eae62aa00337cf7be653bf520d09296e5247f1
Levi Dantas <levibdantas@gmail.com>
2024-09-12 00:57:57 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/dantasdevops/Course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dantasdevops.Course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dantasdevops.Course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+}
