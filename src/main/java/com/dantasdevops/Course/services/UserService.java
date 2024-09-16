70553d3035bd4c9155957f7a8ca362a6e7656df4
Levi Dantas <levibdantas@gmail.com>
2024-09-16 05:57:15 -0300

    Exception handling - update

--- src/main/java/com/dantasdevops/Course/services/UserService.java
+++ src/main/java/com/dantasdevops/Course/services/UserService.java
@@ -12,6 +12,8 @@ import com.dantasdevops.Course.repositories.UserRepository;
 import com.dantasdevops.Course.services.exception.DatabaseException;
 import com.dantasdevops.Course.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
